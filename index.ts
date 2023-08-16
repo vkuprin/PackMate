37a5fa52695932e789fe703840d030f3abcf4fdd
Vitaly Kuprin <vitas_s@inbox.lv>
2023-08-16 14:56:33 +0300

    feat: add some access layers

--- index.ts
+++ index.ts
@@ -50,10 +50,24 @@ async function createAdminUser() {
     });
 }
 
+const verify = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.header('auth-token');
+    if (!token) return res.status(401).json('Access Denied');
+
+    try {
+        req.user = jwt.verify(token, process.env.TOKEN_SECRET || "token") as User;
+        next();
+    } catch (error) {
+        res.status(400).json('Invalid Token');
+    }
+};
+
 openDb();
 
 app.use(cors());
 app.use(express.json());
+app.use('/packages', verify);
+app.use('/upload', verify);
 
 app.post('/login', async (req: Request, res: Response) => {
     const { username, password } = req.body as User;
@@ -74,18 +88,6 @@ app.post('/login', async (req: Request, res: Response) => {
         }
     });
 });
-
-const verify = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('auth-token');
-    if (!token) return res.status(401).json('Access Denied');
-
-    try {
-        req.user = jwt.verify(token, process.env.TOKEN_SECRET || "token") as User;
-        next();
-    } catch (error) {
-        res.status(400).json('Invalid Token');
-    }
-};
 
 app.get('/packages', (req: Request, res: Response) => {
     db.all("SELECT * FROM packages", [], (err, rows) => {
